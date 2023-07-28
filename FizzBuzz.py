ea1d14b9a43278be7a9a1f9637f19e45f8d82b7a
brandonyee-cs <brandonyee.nyc@gmail.com>
2023-07-27 20:35:46 -0400

    FizzBuzz

---
+++ FizzBuzz.py
@@ -0,0 +1,9 @@
+for fb in range (1,101):
+    if (fb % 3 == 0 and fb % 5 == 0):
+        print ("FizzBuzz")
+    elif (fb % 3 == 0):
+        print ("Fizz")
+    elif (fb % 5 == 0):
+        print ("Buzz")
+    else:
+        print ("none")
